6aa4f6f153ddf2370893ad40cb5a60d967e611fe
brunokindt <bruno.kindt@poembyrabb.it>
2018-02-06 12:21:01 +0100

    poc(app): compile react jsx using babel and run in node

---
+++ src/index.js
@@ -0,0 +1,90 @@
+/* eslint-disable quote-props,comma-dangle,no-useless-escape */
+// import React from 'react';
+import React, { Component } from 'react';
+import ReactDOMServer from 'react-dom/server';
+// import App from './App';
+
+
+class TestClass extends Component {
+    static foo = 'bar';
+    render () {
+        return <div>Hi</div>;
+    }
+}
+
+// export default TestClass;
+console.log(TestClass); // logging just for this example
+//
+
+const renderTarget = 'html';
+const id = 1;
+const content = {
+  'sections': [{
+    'id': 14,
+    'type': 'image',
+    'classes': '',
+    'deletable': true,
+    'dynamic': false,
+    'locked': false,
+    'editable': true,
+    'full_width': false,
+    'options': {
+      'title': '&lt;h2&gt;title&lt;\/h2&gt;',
+      'subtitle': '&lt;h3&gt;top.title&lt;\/h3&gt;',
+      'buttonContent': '&lt;a href=&quot;\/&quot;&gt;knop&lt;\/a&gt;',
+      'columnsEnabled': true,
+      'maxColumnCount': 4,
+      'backgroundColor': '',
+      'backgroundImage': '',
+      'buttonHyperlink': '',
+      'colorPickerValue': ''
+    },
+    'columns': [{
+      'id': 26,
+      'type': 'image',
+      'classes': '',
+      'components': [{
+        'id': -4,
+        'alt': '',
+        'url': '',
+        'type': 'image',
+        'tooltip': '',
+        'hyperlink': ''
+      }]
+    }]
+  }]
+};
+
+// const html = 'html';
+
+// const html = ReactDOMServer.renderToStaticMarkup(
+//   <div>Hi</div>
+// );
+
+// const html = ReactDOMServer.renderToStaticMarkup(
+//   <App
+//     id={id}
+//     content={content}
+//     renderTarget={renderTarget}
+//   />
+// );
+
+const html = ReactDOMServer.renderToStaticMarkup(
+  <TestClass
+    id={id}
+    content={content}
+    renderTarget={renderTarget}
+  />
+);
+
+console.log('-------');
+console.log(html);
+console.log('-------');
+
+console.log(process.argv);
+console.log(process.argv0);
+console.log(process.cwd());
+// for (let j = 0; j < process.argv.length; j++) {
+//   console.log(j + ' -> ' + (process.argv[j]));
+// }
+
